5c19856d460dba97c97ee44774f7f7e0383d3ef6
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-22 13:57:46 +0200

    response

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http'); // core module
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === '/') {
+    res.write(
+      '<html><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></html>'
+    );
+    return res.end();
+  }
+  // process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>Hello</html>');
+  res.end();
 }); // runs for every request which reaches our server
 
 server.listen(3000);
