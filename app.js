0fbd7690f4f8c8c31f4b7ee9ddf1bc446683de09
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-22 14:09:41 +0200

    redirecting requests

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const http = require('http'); // core module
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write(
       '<html><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></html>'
     );
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY TEXT');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   // process.exit();
