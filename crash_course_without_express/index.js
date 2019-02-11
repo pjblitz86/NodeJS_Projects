c849800288018c3c67b89782af2b3850892c27ca
Paulius Juknis <paulius.juknis@gmail.com>
2019-02-11 14:36:44 +0200

    sending request and getting back resonse vanilla node

--- crash_course_without_express/index.js
+++ crash_course_without_express/index.js
@@ -3,21 +3,64 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    });
+  //   if (req.url === '/') {
+  //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+  //       if (err) throw err;
+  //       res.writeHead(200, { 'Content-Type': 'text/html' });
+  //       res.end(content);
+  //     });
+  //   }
+
+  //   if (req.url === '/api/users') {
+  //     const users = [{ name: 'Bob Smith', age: 40 }, { name: 'John Doe', age: 30 }];
+  //     res.writeHead(200, { 'Content-Type': 'application/json' });
+  //     res.end(JSON.stringify(users));
+  //   }
+
+  // build file path
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  // Extension of file
+  let extname = path.extname(filePath);
+  // initial content type
+  let contentType = 'text/html';
+
+  // check ext and set content type
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
   }
 
-  if (req.url === '/about') {
-    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-      if (err) throw err;
+  // read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if ((err.code = 'ENOENT')) {
+        // page not found
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf8');
+        });
+      } else {
+        // some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      //success
       res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    });
-  }
+      res.end(content, 'utf8');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
