ab7e85900a9c5f89f5f85c4f888fb271ea551f52
Paulius Juknis <paulius.juknis@gmail.com>
2019-04-11 08:57:46 +0300

    update member put request

--- express_crash_course/routes/api/members.js
+++ express_crash_course/routes/api/members.js
@@ -34,6 +34,24 @@ router.post('/', (req, res) => {
   }
 });
 
+// update member
+router.put('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    const updateMember = req.body;
+    members.forEach(member => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updateMember.name ? updateMember.name : member.name;
+        member.email = updateMember.email ? updateMember.email : member.email;
+        res.json({ msg: 'Member updated', member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `Member with id ${req.params.id} not found` });
+  }
+});
+
 // for reference
 // router.get('/', (req, res) => {
 //   // res.send('<h1>Hello World!!</h1>');
