f5b83b3d65e01ebd943c8d25a143bec2fb18eba6
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-04 14:50:06 +0300

    serving html pages

--- node-learning-main/routes/shop.js
+++ node-learning-main/routes/shop.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>');
+  // path module works for both windows and linux os
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
