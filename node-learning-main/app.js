b2e77272314fb60101e0f13de43c00d1cfab83aa
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-06 13:58:46 +0300

    share data across requests

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
