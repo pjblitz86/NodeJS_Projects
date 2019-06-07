979bf3b6afe0213ed037cabbfb67e071ddace81c
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-07 14:58:45 +0300

    outputting content dynamically with pug

--- node-learning-main/routes/shop.js
+++ node-learning-main/routes/shop.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  // path module works for both windows, mac and linux os
-  res.render('shop');
+  const products = adminData.products;
+  // passing to pug template
+  res.render('shop', {prods: products, docTitle: 'My Shop'});
 });
 
 module.exports = router;
