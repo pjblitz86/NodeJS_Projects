d61199198088f1e17edbe7e970c86a388d3367f5
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-10 14:33:46 +0300

    working with ejs template engine - ma favourite

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const expressHandlebars = require('express-handlebars');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-app.engine('hbs', expressHandlebars({
-  layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
