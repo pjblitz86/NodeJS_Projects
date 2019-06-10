60e402e0500dd7570e32c2176bcc0157ce5d62ed
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-10 15:57:35 +0300

    removing pug and handlebars template code will use ejs from now

--- node-learning-main/routes/admin.js
+++ node-learning-main/routes/admin.js
@@ -1,24 +1,21 @@
-const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product', 
-    path: '/admin/add-product',
-    activeAddProduct: true,
-    formsCSS: true,
-    productCSS: true
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product"
   });
 });
 
-router.post('/add-product', (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect('/');
+  res.redirect("/");
 });
 
 exports.routes = router;
--- node-learning-main/routes/shop.js
+++ node-learning-main/routes/shop.js
@@ -1,21 +1,18 @@
-const path = require('path');
-const rootDir = require('../util/path');
-const express = require('express');
+const path = require("path");
+const rootDir = require("../util/path");
+const express = require("express");
 
 const router = express.Router();
-const adminData = require('./admin');
+const adminData = require("./admin");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   const products = adminData.products;
   // passing to pug template
-  res.render('shop', {
-    prods: products, 
-    pageTitle: 'My Shop', 
-    path: '/', 
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  })
+  res.render("shop", {
+    prods: products,
+    pageTitle: "My Shop",
+    path: "/"
+  });
 });
 
 module.exports = router;
