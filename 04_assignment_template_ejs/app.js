58308f73303495811fed78a0e521a019abcfd215
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-11 10:48:42 +0300

    assignment setup

---
+++ 04_assignment_template_ejs/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000);
