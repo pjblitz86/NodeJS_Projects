05dd127a55c98efe5b523893e7838c1f8cf17dd1
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-13 10:58:27 +0300

    storing and fetching data from a file

--- node-learning-main/controllers/products.js
+++ node-learning-main/controllers/products.js
@@ -1,23 +1,24 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product"
   });
-}
+};
 
 exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect("/");
-}
+};
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    products,
-    pageTitle: "My Shop",
-    path: "/"
+  Product.fetchAll(products => {
+    res.render("shop", {
+      products,
+      pageTitle: "My Shop",
+      path: "/"
+    });
   });
-}
+};
