8f293be86b987221ed3013fd539ff339ccceed8e
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-14 14:28:50 +0300

    work on edit product functionality

--- node-learning-main/routes/admin.js
+++ node-learning-main/routes/admin.js
@@ -7,4 +7,8 @@ router.get("/products", adminController.getProducts);
 
 router.post("/add-product", adminController.postAddProduct);
 
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product");
+
 module.exports = router;
