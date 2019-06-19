7d0cee4b2b11112fcf6c2446acb18af4c70128cc
Paulius_Juknis <paulius.juknis@gmail.com>
2019-06-19 15:50:19 +0300

    deleting products

--- node-learning-main/controllers/admin.js
+++ node-learning-main/controllers/admin.js
@@ -17,7 +17,10 @@ exports.postAddProduct = (req, res, next) => {
     imageUrl: imageUrl,
     description: description
   })
-    .then(result => console.log("created product"))
+    .then(result => {
+      console.log("created product");
+      res.redirect("/admin/products");
+    })
     .catch(err => console.log(err));
 };
 
@@ -78,6 +81,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log("product deleted");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log(err));
 };
