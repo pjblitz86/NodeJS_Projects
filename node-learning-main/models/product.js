0d0f18adc542f8c6862fae8387e53d64f6149b19
Paulius_Juknis <paulius.juknis@gmail.com>
2019-06-19 11:03:10 +0300

    fetching single product by id from mySQL db

--- node-learning-main/models/product.js
+++ node-learning-main/models/product.js
@@ -10,7 +10,12 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.imagUrl, this.description]
+    );
+  }
 
   static deleteById(id) {}
 
@@ -18,5 +23,7 @@ module.exports = class Product {
     return db.execute("SELECT * FROM products");
   }
 
-  static findById(id) {}
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
+  }
 };
