df1d8304777985ea6752e7a6dfa50d0e7eb191b0
Paulius_Juknis <paulius.juknis@gmail.com>
2019-06-19 13:30:36 +0300

    syncinc model to db

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -10,7 +10,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,4 +19,10 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
