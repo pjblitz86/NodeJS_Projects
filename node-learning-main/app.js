6f8d212774c2bcfd5369ae74f8eb6338a9694dbd
Paulius_Juknis <paulius.juknis@gmail.com>
2019-06-20 10:33:28 +0300

    create dummy user and relation between product

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -12,17 +12,43 @@ const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 
+const Product = require("./models/product");
+const User = require("./models/user");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true }) to override tables if no relation created
   .sync()
   .then(result => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      User.create({ name: "Paul", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => console.log(err));
