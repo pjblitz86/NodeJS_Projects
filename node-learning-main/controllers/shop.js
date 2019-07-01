1231f717c07ac989e974b507b62dd8177aa8b5c6
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-01 16:07:12 +0300

    displaying cart items

--- node-learning-main/controllers/shop.js
+++ node-learning-main/controllers/shop.js
@@ -40,17 +40,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render("shop/cart", {
-            path: "/cart",
-            pageTitle: "Your Cart",
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -61,7 +56,7 @@ exports.postCart = (req, res, next) => {
     .then(product => {
       return req.user.addToCart(product);
     })
-    .then(result => console.log(result));
+    .then(result => res.redirect("/cart"));
 
   // let fetchedCart;
   // let newQuantity = 1;
