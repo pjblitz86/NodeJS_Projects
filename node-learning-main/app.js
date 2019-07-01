8eff96dca69aa27ddf22617ead8d168ac08ec0a2
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-01 08:58:28 +0300

    fetching all products

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 
 const mongoConnect = require("./util/database").mongoConnect;
@@ -17,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
-// app.use(errorController.get404);
+app.use(shopRoutes);
+app.use(errorController.get404);
 
 mongoConnect(() => {
   app.listen(3000);
