3dc564d3fba49d57a9ae658deef4ed74139831bf
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-02 15:36:18 +0300

    populating and displaying cart

--- node-learning-main/models/user.js
+++ node-learning-main/models/user.js
@@ -47,6 +47,26 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 };
 
+// userSchema.methods.getCart = function(product) {
+//   const productIds = this.cart.items.map(i => {
+//     return i.productId;
+//   });
+//   return db
+//     .collection("products")
+//     .find({ _id: { $in: productIds } })
+//     .toArray()
+//     .then(products => {
+//       return products.map(p => {
+//         return {
+//           ...p,
+//           quantity: this.cart.items.find(i => {
+//             return i.productId.toString() === p._id.toString();
+//           }).quantity
+//         };
+//       });
+//     });
+// };
+
 module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require("mongodb");
