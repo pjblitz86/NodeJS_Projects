4df4c884cdc054b71d4298e01dce58cc565f5ceb
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-02 15:44:02 +0300

    delete cart items

--- node-learning-main/models/user.js
+++ node-learning-main/models/user.js
@@ -47,25 +47,13 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 };
 
-// userSchema.methods.getCart = function(product) {
-//   const productIds = this.cart.items.map(i => {
-//     return i.productId;
-//   });
-//   return db
-//     .collection("products")
-//     .find({ _id: { $in: productIds } })
-//     .toArray()
-//     .then(products => {
-//       return products.map(p => {
-//         return {
-//           ...p,
-//           quantity: this.cart.items.find(i => {
-//             return i.productId.toString() === p._id.toString();
-//           }).quantity
-//         };
-//       });
-//     });
-// };
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 
