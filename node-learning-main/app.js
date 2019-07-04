90403b40ef08e45545a0beb6e508435b938b6680
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-04 13:05:07 +0300

    creating login form

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -10,6 +10,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 const errorController = require("./controllers/error");
 
 const User = require("./models/user");
@@ -28,6 +29,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
