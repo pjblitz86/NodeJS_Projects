52ccc785a3f854b153eea64e025ad0a7f404867c
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-05 10:10:54 +0300

    fixing session bugs and cart page

--- node-learning-main/controllers/shop.js
+++ node-learning-main/controllers/shop.js
@@ -43,7 +43,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.session.user
+  req.user
     .populate("cart.items.productId")
     .execPopulate()
     .then(user => {
@@ -62,14 +62,14 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then(result => res.redirect("/cart"));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.session.user
+  req.user
     .removeFromCart(prodId)
     .then(result => {
       res.redirect("/cart");
@@ -78,7 +78,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  req.session.user
+  req.user
     .populate("cart.items.productId")
     .execPopulate()
     .then(user => {
@@ -87,22 +87,22 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.session.user.name,
-          userId: req.session.user
+          name: req.user.name,
+          userId: req.user
         },
         products: products
       });
       return order.save();
     })
     .then(result => {
-      return req.session.user.clearCart();
+      return req.user.clearCart();
     })
     .then(() => res.redirect("/orders"))
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ "user.userId": req.session.user })
+  Order.find({ "user.userId": req.user })
     .then(orders => {
       res.render("shop/orders", {
         path: "/orders",
