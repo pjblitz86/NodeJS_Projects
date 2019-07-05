790cd50b0154d08e78fa8a05b4f51d8dcc39de40
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-05 08:57:18 +0300

    storing session in mongodb

--- node-learning-main/app.js
+++ node-learning-main/app.js
@@ -3,8 +3,15 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const MONGODBURI =
+  "mongodb+srv://pjblitz86:pjblitz86@cluster0-c1rev.mongodb.net/node-shop?retryWrites=true&w=majority";
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODBURI,
+  collection: "sessions"
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,7 +25,14 @@ const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("5d1b3921bd5d3d213cd71c23")
@@ -35,10 +49,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://pjblitz86:pjblitz86@cluster0-c1rev.mongodb.net/node-shop?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(MONGODBURI, { useNewUrlParser: true })
   .then(result => {
     console.log("mongo connected");
     User.findOne().then(user => {
