47d7f454d4c9bea9483ef491cffee368c9a4ba0e
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-08 11:29:12 +0300

    created a password reset form

--- node-learning-main/routes/auth.js
+++ node-learning-main/routes/auth.js
@@ -9,5 +9,7 @@ router.post("/signup", authController.postSignup);
 router.post("/logout", authController.postLogout);
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
+router.post("/reset", authController.postReset);
+router.get("/reset/:token", authController.getNewPassword);
 
 module.exports = router;
