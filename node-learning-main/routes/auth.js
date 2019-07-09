bf3b3d465167e0a170d474b74cff3095348872b6
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-09 08:43:11 +0300

    checking for confirmPassword match with custom validator

--- node-learning-main/routes/auth.js
+++ node-learning-main/routes/auth.js
@@ -8,10 +8,23 @@ router.get("/signup", authController.getSignup);
 router.post("/login", authController.postLogin);
 router.post(
   "/signup",
-  [check("email")
-    .isEmail()
-    .withMessage("Please enter a valid email"),
-  body("password", "Please enter a password with numbers and text with at least 5 characters").isLength({min: 5}).isAlphanumeric()],
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email"),
+    body(
+      "password",
+      "Please enter a password with numbers and text with at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match");
+      }
+      return true;
+    })
+  ],
   authController.postSignup
 );
 router.post("/logout", authController.postLogout);
