bd7557781ea89053d7379aef88bed935e17d45f9
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-09 11:22:08 +0300

    add validation for creating a product

--- node-learning-main/controllers/admin.js
+++ node-learning-main/controllers/admin.js
@@ -1,15 +1,35 @@
 const Product = require("../models/product");
+const { validationResult } = require("express-validator");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Edit Product",
     path: "/admin/add-product",
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      },
+      errorMessage: errors.array()[0].msg
+    });
+  }
   const product = new Product({
     title: title,
     price: price,
@@ -41,7 +61,9 @@ exports.getEditProduct = (req, res, next) => {
           pageTitle: "Edit Product",
           path: "/admin/edit-product",
           editing: editMode,
-          product: product
+          product: product,
+          hasError: false,
+          errorMessage: null
         });
       }
     })
--- node-learning-main/routes/admin.js
+++ node-learning-main/routes/admin.js
@@ -2,15 +2,50 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
+const { body } = require("express-validator");
 
 router.get("/add-product", isAuth, adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
 
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  isAuth,
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body("imageUrl")
+      .isURL()
+      .trim(),
+    body("price").isFloat(),
+    body("description")
+      .isLength({ min: 4, max: 255 })
+      .trim()
+  ],
+  adminController.postAddProduct
+);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  isAuth,
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body("imageUrl")
+      .isURL()
+      .trim(),
+    body("price").isFloat(),
+    body("description")
+      .isLength({ min: 4, max: 255 })
+      .trim()
+  ],
+  adminController.postEditProduct
+);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
