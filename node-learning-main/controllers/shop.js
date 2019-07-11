2d958c9dfd80fa2e4cae639c53f97c6fd747d428
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-11 09:17:31 +0300

    set file type header

--- node-learning-main/controllers/shop.js
+++ node-learning-main/controllers/shop.js
@@ -116,11 +116,12 @@ exports.getOrders = (req, res, next) => {
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
   const invoiceName = `invoice-${orderId}.pdf`;
-  const invoicePath = path.join('data', 'invoices', invoiceName);
+  const invoicePath = path.join("data", "invoices", invoiceName);
   fs.readFile(invoicePath, (err, data) => {
-    if(err) {
+    if (err) {
       return next(err);
     }
+    res.setHeader("Content-Type", "application/pdf");
     res.send(data);
   });
-}
+};
