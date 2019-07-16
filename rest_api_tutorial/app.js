7e58d3b276aea7248dc456914807d68288c43170
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-16 11:00:20 +0300

    setting up mongodb and post model

--- rest_api_tutorial/app.js
+++ rest_api_tutorial/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const feedRoutes = require("./routes/feed");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+const MONGODBURI =
+  "mongodb+srv://pjblitz86:pjblitz86@cluster0-c1rev.mongodb.net/node-api-posts?retryWrites=true&w=majority";
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
@@ -18,4 +21,10 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8040, () => console.log("server started"));
+mongoose
+  .connect(MONGODBURI, { useNewUrlParser: true })
+  .then(result => {
+    app.listen(8040, () => console.log("server started"));
+    console.log("mongo connected");
+  })
+  .catch(err => console.log(err));
