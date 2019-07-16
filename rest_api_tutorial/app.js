4a5479173e74da97f5250abffd8806514e770b94
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-16 15:29:44 +0300

    make image static and some error handling

--- rest_api_tutorial/app.js
+++ rest_api_tutorial/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const feedRoutes = require("./routes/feed");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -8,6 +9,7 @@ const MONGODBURI =
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "iamges")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +22,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(MONGODBURI, { useNewUrlParser: true })
--- rest_api_tutorial/controllers/feed.js
+++ rest_api_tutorial/controllers/feed.js
@@ -21,9 +21,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ message: "validation failed", errors: errors.array() });
+    const error = new Error("validation failed");
+    error.statusCode = 422;
+    throw error;
   }
   const { title, content } = req.body;
   const post = new Post({
@@ -41,5 +41,10 @@ exports.createPost = (req, res, next) => {
         post: result
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
