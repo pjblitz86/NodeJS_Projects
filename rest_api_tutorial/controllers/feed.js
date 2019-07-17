1e38259c4c7d20b2b07ea1191da79b5aa4678378
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-17 14:21:26 +0300

    adding pagination

--- rest_api_tutorial/controllers/feed.js
+++ rest_api_tutorial/controllers/feed.js
@@ -4,9 +4,25 @@ const fs = require("fs");
 const path = require("path");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then(posts => {
-      res.status(200).json({ message: "Fetched posts", posts: posts });
+      res
+        .status(200)
+        .json({
+          message: "Fetched posts",
+          posts: posts,
+          totalItems: totalItems
+        });
     })
     .catch(err => {
       if (!err.statusCode) {
