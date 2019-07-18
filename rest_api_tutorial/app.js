4e40f35736b19c59f82997a13fb20969fb728731
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-18 10:34:54 +0300

    signing users up implemented

--- rest_api_tutorial/app.js
+++ rest_api_tutorial/app.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
   console.log(err);
   const status = err.statusCode || 500;
   const message = err.message;
-  const data = error.data;
+  const data = err.data;
   res.status(status).json({ message: message, data: data });
 });
 
--- rest_api_tutorial/controllers/auth.js
+++ rest_api_tutorial/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 
 exports.signup = (req, res, next) => {
@@ -10,4 +11,23 @@ exports.signup = (req, res, next) => {
     throw error;
   }
   const { email, name, password } = req.body;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        name: name
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({ message: "User created", userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
