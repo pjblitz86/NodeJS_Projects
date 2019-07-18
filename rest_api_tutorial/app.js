6cb97165d1d83885f9932735e51b8d1b7c86af14
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-18 10:17:18 +0300

    adding user signup validation

--- rest_api_tutorial/app.js
+++ rest_api_tutorial/app.js
@@ -55,7 +55,8 @@ app.use((err, req, res, next) => {
   console.log(err);
   const status = err.statusCode || 500;
   const message = err.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
---
+++ rest_api_tutorial/controllers/auth.js
@@ -0,0 +1,13 @@
+const User = require("../models/user");
+const { validationResult } = require("express-validator");
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const { email, name, password } = req.body;
+};
